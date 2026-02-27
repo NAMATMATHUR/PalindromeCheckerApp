0fc6c0f42cf198df38c5e3238ed5477449b7360a
NAMATMATHUR <mathurnamat28@gmail.com>
2026-02-27 15:53:29 +0530

    UC4: Ignore spaces and special characters

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -8,19 +8,22 @@ public class PalindromeCheckerApp {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a word: ");
-        String word = scanner.nextLine();
+        System.out.print("Enter a sentence: ");
+        String input = scanner.nextLine();
+
+        // Remove spaces and special characters
+        String cleaned = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
         String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
+        for (int i = cleaned.length() - 1; i >= 0; i--) {
+            reversed += cleaned.charAt(i);
         }
 
-        if (word.equalsIgnoreCase(reversed)) {
-            System.out.println(word + " is a Palindrome");
+        if (cleaned.equals(reversed)) {
+            System.out.println("It is a Palindrome");
         } else {
-            System.out.println(word + " is NOT a Palindrome");
+            System.out.println("It is NOT a Palindrome");
         }
 
         scanner.close();
